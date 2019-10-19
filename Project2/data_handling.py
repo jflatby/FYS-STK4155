0f01575287ab41c06261a4224ebb55635fac5f4b
Linueks <linueks@gmail.com>
2019-10-19 16:13:24 +0200

    some changes

--- Project2/data_handling.py
+++ Project2/data_handling.py
@@ -7,7 +7,7 @@ import argparse
 
 
 
-def read_data(filename):
+def read_data(filtered=True):
     """
     column names:
 
@@ -18,9 +18,26 @@ def read_data(filename):
     'defaultPaymentNextMonth'
     """
 
+
     nanDict = {}
-    df = pd.read_excel(filename, header=1, skiprows=0, index_col=0, na_values=nanDict)
-    df.rename(index=str, columns={'default payment next month': 'defaultPaymentNextMonth'}, inplace=True)
+    if filtered:
+        df = pd.read_excel('filtered_credit_card_data.xls', header=0, skiprows=0, index_col=0, na_values=nanDict)
+    else:
+        df = pd.read_excel('default_of_credit_card_clients.xls', header=1, skiprows=0, index_col=0, na_values=nanDict)
+        df.rename(index=str, columns={'default payment next month': 'defaultPaymentNextMonth'}, inplace=True)
+
+    return df
+
+
+
+def filter_data(df, filename='filtered_credit_card_data.xls'):
+    """Removes unnecessary / unwanted values from datasets and saves to new file
+
+    Parameters:
+    df (pandas dataframe): dataframe containing dataset of credit card
+                                    default data.
+    filename (str): filename of file to save to. 
+    """
 
     # Drop unnecessary values
     df = df.drop(df[(df.MARRIAGE == 0)].index)
@@ -28,11 +45,27 @@ def read_data(filename):
                     (df.EDUCATION == 5) |
                     (df.EDUCATION == 6)].index)
 
-    return df
+    df.to_excel(filename)
+
 
 
 
 def generate_scatter(dataframe, category_1, category_2):
+    """Generates scatter plot of the two categories supplied. Will label axes
+    and supply title based on categories input.
+
+    Parameters:
+    dataframe (pandas dataframe): dataframe containing dataset of credit card
+                                    default data.
+
+    category_1 (str): First category for comparison
+    category_2 (str): Second category for comparison
+
+
+    """
+
+
+
     plt.scatter(dataframe[category_1], dataframe[category_2], alpha=0.4, edgecolors='w')
     plt.xlabel(category_1)
     plt.ylabel(category_2)
@@ -42,6 +75,20 @@ def generate_scatter(dataframe, category_1, category_2):
 
 
 def generate_jointplot(dataframe, category_1, category_2):
+    """Function using seaborn module for plotting a joint plot of two
+    categories present in credit card dataset
+
+    Parameters:
+    dataframe (pandas dataframe): dataframe containing dataset of credit card
+                                    default data.
+
+    category_1 (str): First category for comparison
+    category_2 (str): Second category for comparison
+
+
+    """
+
+
     joint_plot = sbr.jointplot(x=f'{category_1}', y=f'{category_2}',
                             data=dataframe, kind='kde', space=0,
                             height=5, ratio=4)
@@ -49,13 +96,32 @@ def generate_jointplot(dataframe, category_1, category_2):
 
 
 
+def generate_histogram(dataframe, category):
+    """Function using seaborn module for plotting of histogram for the given
+    category present in the credit card dataset
+
+    Parameters:
+    dataframe (pandas dataframe): dataframe containing dataset of credit card
+                                    default data.
+
+    category (str): First category for comparison
+
+
+    """
+
+
+    histogram = sbr.distplot(dataframe[category])
+    plt.show()
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    dataframe = read_data('default of credit card clients.xls')
-    dataframe.to_excel('filtered credit card data.xls')
-    #values, amount = np.unique(dataframe['EDUCATION'], return_counts=True)
-    #print(values, amount)
+    parser.add_argument('--filtered', help='uses filtered credit card data', action='store_true')
+    args = parser.parse_args()
+    dataframe = read_data(filtered=args.filtered)
 
 
-    #generate_scatter(dataframe, 'LIMIT_BAL', 'AGE')
-    #generate_jointplot(dataframe, 'LIMIT_BAL', 'AGE')
+    #generate_scatter(dataframe, 'MARRIAGE', 'AGE')
+    #generate_jointplot(dataframe, 'MARRIAGE', 'AGE')
+    #generate_histogram(dataframe, 'AGE')
